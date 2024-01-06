45d0adb6ca3e5fbc284f59791151cd058fded05d
oangia <blogphp.biz@gmail.com>
2024-01-06 02:00:35 -0500

    U

--- app/Models/User.php
+++ app/Models/User.php
@@ -1,25 +1,16 @@
 <?php
 namespace App\Models;
 
-use Illuminate\MySQL\Model;
+use Illuminate\Database\MongoDB\Model;
 
 class User extends Model {
-    public static $table = 'users';
+    public static $database = 'test';
+    public static $collection = 'users';
+    protected static $encrypt = ['email'];
 
-    public static function current() {
-        $uuid = isset($_COOKIE['_xp']) ? $_COOKIE['_xp'] : User::generateUUID();
-        $user = User::findOrCreate([
-            'uuid' => $uuid
-        ], [
-            'email' => $uuid . '@gmail.com',
-            'name' => $uuid
-        ]);
-        return $user;
-    }
+    protected $email;
 
-    public static function generateUUID() {
-        $uuid = md5(get_user_ip() . get_user_agent() . microtime());
-        setcookie('_xp', $uuid, time() + 365 * 24 * 60 * 60, '/');
-        return $uuid;
+    public function map($data) {
+        $this->email = $data['email'];
     }
 }
